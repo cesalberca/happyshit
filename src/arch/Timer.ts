fa01e2981ddb87c1097631029f8dae1547f960a7
César Alberca <cesalberca@gmail.com>
2018-11-18 20:38:54 +0100

    Use Maybe instead of undefined

--- src/arch/Timer.ts
+++ src/arch/Timer.ts
@@ -1,3 +1,5 @@
+import { Maybe } from './domain/Maybe'
+
 export class Time {
   public get seconds(): number {
     return this._seconds
@@ -21,10 +23,10 @@ export interface Clock {
 
 export class Timer {
   private _time: Time = new Time(0)
-  private currentClock: number | undefined
+  private _currentClock: Maybe<number> = Maybe.none()
 
-  private _until: Time | undefined
-  private _onTimerRuns: (() => any) | undefined
+  private _until: Maybe<Time> = Maybe.none()
+  private _onTimerRuns: Maybe<(() => any)> = Maybe.none()
 
   public constructor(private readonly clock: Clock) {
     this.clock = clock
@@ -32,32 +34,36 @@ export class Timer {
 
   public start() {
     this._time = new Time(0)
-    this.currentClock = this.clock.start(() => {
-      this._time = new Time(this.time.seconds + 1)
-      this.shouldStopTime()
-    })
+    this._currentClock = Maybe.fromValue(
+      this.clock.start(() => {
+        this._time = new Time(this.time.seconds + 1)
+        this.stopTimeIfNeeded()
+      })
+    )
 
     return this
   }
 
   public until(time: Time, onTimerRuns: () => any) {
-    this._until = time
-    this._onTimerRuns = onTimerRuns
+    this._until = Maybe.fromValue(time)
+    this._onTimerRuns = Maybe.fromValue(onTimerRuns)
   }
 
   public stop() {
-    this.clock.stop(this.currentClock as number)
-    this.currentClock = undefined
+    this.clock.stop(this._currentClock.getOrElse(0))
+    this._currentClock = Maybe.none()
   }
 
   public get time(): Time {
     return this._time
   }
 
-  private shouldStopTime() {
-    if (this._until && this._time.seconds === this._until.seconds) {
+  private stopTimeIfNeeded() {
+    if (
+      this._time.seconds === this._until.getOrElse(Time.seconds(-1)).seconds
+    ) {
       this.stop()
-      this._onTimerRuns!()
+      this._onTimerRuns.getOrElse(() => {})()
     }
   }
 }
